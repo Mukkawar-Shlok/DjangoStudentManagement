a08d423e9a15d4e73261127cd63caa0bb583661e
Shlok Mukkawar <shlokmukkawar2001@gmail.com>
2024-03-08 14:37:17 +0530

    deply changes

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -162,6 +162,9 @@ EMAIL_USE_TLS = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-prod_db = dj_database_url.config('postgres://sample_cruv_user:pX0AXexTxeg7MIuFmLmMZ8epVd6u0lsh@dpg-cnlai0gl6cac73edrfkg-a.singapore-postgres.render.com/sample_cruv', conn_max_age=500)
+prod_db = dj_database_url.config(
+    default='postgres://sample_cruv_user:pX0AXexTxeg7MIuFmLmMZ8epVd6u0lsh@dpg-cnlai0gl6cac73edrfkg-a.singapore-postgres.render.com/sample_cruv',
+    conn_max_age=500
+)
+# prod_db = dj_database_url.config('postgres://sample_cruv_user:pX0AXexTxeg7MIuFmLmMZ8epVd6u0lsh@dpg-cnlai0gl6cac73edrfkg-a.singapore-postgres.render.com/sample_cruv', conn_max_age=500)
 DATABASES['default'].update(prod_db)
